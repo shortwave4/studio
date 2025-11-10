85a6e1ca1ad1db8f68b52008ec48ae3c04aa2cd9
Fahad Khan <newpwafk15520041@gmail.com>
2025-11-10 15:19:04 +0000

    Ye announcement ka rehne do isme error ara hai hatado

--- src/app/(main)/discover/page.tsx
+++ src/app/(main)/discover/page.tsx
@@ -1,10 +1,8 @@
 import DiscoverUsers from "@/components/discover-users";
-import Announcements from "@/components/announcements";
 
 export default function DiscoverPage() {
   return (
     <div className="container mx-auto">
-       <Announcements />
       <div className="mb-8 mt-8 bg-card/80 p-6 rounded-lg shadow-lg backdrop-blur-sm">
         <h1 className="text-3xl font-bold font-headline tracking-tight">Discover People</h1>
         <p className="text-muted-foreground">
